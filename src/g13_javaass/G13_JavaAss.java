46f2e9e77d94f717fc582c4344fb9550f8c49da5
Thenoobdude26 <tatendacoolboy@gmail.com>
2025-07-21 04:29:32 +0800

    first commit

---
+++ src/g13_javaass/G13_JavaAss.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package g13_javaass;
+
+/**
+ *
+ * @author t
+ */
+public class G13_JavaAss {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
